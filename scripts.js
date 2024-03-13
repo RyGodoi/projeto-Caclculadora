73f9ad4592c4212a990290ed39ddbf06a7b53f25
Ryan Godoi da Silva <ryan.godoi.dasilva@gmail.com>
2024-03-12 23:22:06 -0300

    primeiro commit

--- scripts.js
+++ scripts.js
@@ -62,7 +62,7 @@ class Calculator {
       case "÷":
         result = _previousOperand / _currentOperand;
         break;
-      case "*":
+      case "x":
         result = _previousOperand * _currentOperand;
         break;
       default:
